b451ba4949163d3cbdac057342d5422eccfc55f6
ParitoshSahu2 <paritosh9826@gmail.com>
2025-05-29 16:53:02 +0530

    commit

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ import UserClass from "./UserClass";
 const About=()=>{
 return(
 <div>
-    <h1>this is about section</h1>
+    <h1>About section</h1>
     <User name={"Paritosh Sahu(Function)"}/>
     <UserClass  name={"Paritosh Sahu(class)"} location={"Indore(class)"}/>
 </div>
